2f3f16cca67bdc3df2160c63998a254f083d5d3b
sgarciaanato <sgarciaanato@gmail.com>
2024-04-19 13:59:35 -0400

    Initial Commit

---
+++ Walmart Chile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Walmart Chile
+//
+//  Created by Samuel García on 19-04-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
